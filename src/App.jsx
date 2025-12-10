50dfb1ab7e28e951a9a871898b3c674f05f0cd3a
rahul-anantacloud <rahul.goswami@anantacloud.com>
2025-12-10 14:01:28 +0530

    default structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,9 @@
-import Header from './components/layout/Header';
-import Footer from './components/layout/Footer';
-import Hero from './components/sections/Hero';
-import Features from './components/sections/Features';
-import UseCases from './components/sections/UseCases';
-import Testimonials from './components/sections/Testimonials';
-import Stats from './components/sections/Stats';
-import Providers from './components/sections/Providers';
-import CTA from './components/sections/CTA';
 
 function App() {
   return (
-    <div className="min-h-screen w-full bg-[#030921]">
-      <Header />
-      <main className="w-full">
-        <Hero />
-        <Stats />
-        <Features />
-        <UseCases />
-        <Providers />
-        <Testimonials />
-        <CTA />
-      </main>
-      <Footer />
-    </div>
+   <>
+   <div>hello</div>
+   </>
   );
 }
 
